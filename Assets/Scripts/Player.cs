39ed396b51d43642994038697dfb1bbbf4b9355b
IsisTinti <94065141+IsisTinti@users.noreply.github.com>
2024-11-15 21:25:55 -0300

    ataque y daño v.1
    los codigos estan incompletos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IDamageable
 {
-
+    [SerializeField]
+    private AttackRadius AttackRadius;
+    private Coroutine LookCoroutine;
     public Rigidbody rb;
     public float speed;
     public float forceJump;
@@ -16,10 +18,25 @@ public class Player : MonoBehaviour
     public int damage;
     public int upgradeLifeQuantity;
     public Animator animator;
+    //  Alternativa por si el constructor life no funciona
+    //----------------------------------------------------------
+
+    [SerializeField]
+    private int _health = 100;
+
+    private const string ATTACK_TRIGGER = "Attack";
+    //--------------------------------------------------
+
+    private void Awake()
+    {
+        AttackRadius.OnAttack += OnAttack;
+    }
+
+
 
     void Start()
     {
-        _movement = new Movement(transform, rb, speed,forceJump);
+        _movement = new Movement(transform, rb, speed, forceJump);
         _control = new Control(_movement);
         _life = new Life(maxlife, life, damage, upgradeLifeQuantity);
 
@@ -49,6 +66,47 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void OnAttack(IDamageable Target)
+    {
+        animator.SetTrigger(ATTACK_TRIGGER);
+
+        if (LookCoroutine != null)
+        {
+            StopCoroutine(LookCoroutine);
+        }
+
+        LookCoroutine = StartCoroutine(LookAt(Target.GetTransform()));
+    }
+
+    private IEnumerator LookAt(Transform Target)
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(Target.position - transform.position);
+        float time = 0;
+
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, time);
+            time += Time.deltaTime * 2;
+            yield return null;
+        }
+
+        transform.rotation = lookRotation;
+
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
     public void SpeedUp(float add)
     {
         _movement.SpeedUp(add);
@@ -64,13 +122,15 @@ public class Player : MonoBehaviour
         _movement.ArtificialOnCollisionEnter(collision);
     }
 
-   /* private void OnTriggerEnter(Collider other)
-    {
-        var powerUp = other.GetComponent<PowerUp>();
 
-        if (powerUp != null)
-            powerUp.Get(this);
-    }
-   */
+    //---------------PRUEBA POWERUP--------------------------
+    /* private void OnTriggerEnter(Collider other)
+     {
+         var powerUp = other.GetComponent<PowerUp>();
+
+         if (powerUp != null)
+             powerUp.Get(this);
+     }
+    */
 
 }
